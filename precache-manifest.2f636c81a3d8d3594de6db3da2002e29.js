cadcd0a61b58a0eeb09ecd7b17b983186b6cbfd4
bakunovdo <bakunov.do@gmail.com>
2020-09-28 17:51:51 +0300

    Updates

---
+++ precache-manifest.2f636c81a3d8d3594de6db3da2002e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725cdeeb0257e2824aaa0eaefb07a7c5",
+    "url": "/cryptoCurrencies/index.html"
+  },
+  {
+    "revision": "375e1d7a1f7b6929d9a6",
+    "url": "/cryptoCurrencies/static/css/main.13f9bb54.chunk.css"
+  },
+  {
+    "revision": "145d1e554a4e3ae6e0d5",
+    "url": "/cryptoCurrencies/static/js/2.a6d07881.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cryptoCurrencies/static/js/2.a6d07881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375e1d7a1f7b6929d9a6",
+    "url": "/cryptoCurrencies/static/js/main.88c5fd29.chunk.js"
+  },
+  {
+    "revision": "3d112bd8313333648d1c",
+    "url": "/cryptoCurrencies/static/js/runtime-main.51e736c8.js"
+  }
+]);
